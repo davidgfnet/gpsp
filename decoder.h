b93d3fe6071e42c60cadec57c09dc6dafed1e1ca
David Guillen Fandos <david@davidgf.net>
2024-02-21 23:59:18 +0100

    [interp] Big rewrite, Thumb completed ARM only memory ops missing

--- decoder.h
+++ decoder.h
@@ -35,7 +35,7 @@ public:
     return (opcode & 0x80) ? -i : i;
   }
     u32 imm = (opcode & 0x7F);
-  // Immediate used for shift/rotate operations
+  // Immediate used for shift/rotate operations, as well as load/store
   u32 imm5() const {
     return (opcode >> 6) & 0x1F;
   }
@@ -68,6 +68,12 @@ public:
   u8 rptr() const {
     return (opcode >> 8) & 0x7;   // Base reg for ldm/stm
   }
+  u32 ro() const {
+    return (opcode >> 6) & 0x7;
+  }
+  u32 rb() const {
+    return (opcode >> 3) & 0x7;
+  }
 
   // Hireg resgister encodings
   u8 rs_hi() const {
@@ -94,6 +100,86 @@ public:
   }
   u32 abr_offset_lo() const {
     return (opcode & 0x07FF) << 1;
+  }
+};
+
+
+class ARMInst {
+public:
+  u32 opcode;
+
+  ARMInst(u32 opcode)
+   : opcode(opcode) {}
+
+  u32 cond() const {
+    return (opcode >> 28) & 0xF;
+  }
+  u32 op8() const {
+    return (opcode >> 20) & 0xFF;
+  }
+
+  // Immediates
+  u32 imm8() const {
+    return opcode & 0xFF;
+  }
+  u32 rot4() const {
+    return (opcode >> 8) & 0xF;
+  }
+  u32 rot_imm8() const {
+    u32 sa = rot4() * 2;
+    return rotr32(imm8(), sa);
+  }
+
+  // Usual register info
+  u32 rn() const {
+    return (opcode >> 16) & 0xF;
+  }
+  u32 rd() const {
+    return (opcode >> 12) & 0xF;
+  }
+  u32 rs() const {
+    return (opcode >> 8) & 0xF;
+  }
+  u32 rm() const {
+    return opcode & 0xF;
+  }
+  u32 rdhi() const {
+    return (opcode >> 16) & 0xF;
+  }
+  u32 rdlo() const {
+    return (opcode >> 12) & 0xF;
+  }
+
+  // Operand 2 bits and modes
+  bool op2imm() const {
+    // whether the register is shifted/rotated by an immediate or another reg.
+    return (opcode & 0x10) == 0;
+  }
+  u32 op2smode() const {
+    // Shift mode by the register op2 (lsl/lsr/asr/ror).
+    return (opcode >> 5) & 0x3;
+  }
+  u32 op2sa() const {
+    // Immediate amount of shift for op2
+    return (opcode >> 7) & 0x1F;
+  }
+
+  // Branch
+  s32 br_offset() const {
+    // Returns already scaled (x4) offset for the branch.
+    s32 r = opcode << 8;
+    return r >> 6;
+  }
+
+  // MSR / MRS
+  u32 psr_field() const {
+    // Returns bits for flags and control only (no extension nor status)
+    return ((opcode >> 16) & 1) | ((opcode >> 18) & 2);
+  }
+
+  // Memory instructions
+  u16 rlist() const {
+    return opcode & 0xFFFF;   // Reg list for STM/LDM
   }
 };
 

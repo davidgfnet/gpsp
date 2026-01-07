a3e68e09729d35555d506da6d50c92ea1f75aab5
David Guillen Fandos <david@davidgf.net>
2026-01-07 17:28:40 +0100

    [dynarec] More ARM rewrite

--- arm/arm32_codegen.h
+++ arm/arm32_codegen.h
@@ -44,9 +44,14 @@ typedef enum {
   armcg_regsp = 13,
   armcg_reglr = 14,
   armcg_regpc = 15,
+
+  armcg_reginvalid = 16,
 } armcg_regnum;
 
 typedef enum {
+  armgc_mul   = 0x0,
+  armgc_mla   = 0x1,
+
   armgc_umull = 0x4,
   armgc_umlal = 0x5,
   armgc_smull = 0x6,
@@ -97,77 +102,92 @@ public:
 
   // Performs an ALU operation (with/out flag setting) with some immediate shift amount
   template <armcg_op op, FlagOperation flg>
-  inline void emit_alu_reg_immshift(uint32_t rd, uint32_t rn, uint32_t rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
+  inline void emit_alu_reg_immshift(armcg_regnum rd, armcg_regnum rn, armcg_regnum rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
     emit_aluop_reg<op, flg>(rd, rn, op2regimm(rm, smode, samount));
   }
 
   // Performs an ALU operation (with/out flag setting) with some reg shift amount
   template <armcg_op op, FlagOperation flg>
-  inline void emit_alu_reg_regshift(uint32_t rd, uint32_t rn, uint32_t rm, uint32_t smode, uint32_t rs) {
+  inline void emit_alu_reg_regshift(armcg_regnum rd, armcg_regnum rn, armcg_regnum rm, uint32_t smode, uint32_t rs) {
     emit_aluop_reg<op, flg>(rd, rn, op2regreg(rm, smode, rs));
   }
 
   // Performs an ALU operation (with/out flag setting) with some immediate value
   template <armcg_op op, FlagOperation flg>
-  inline void emit_alu_imm(uint32_t rd, uint32_t rn, uint32_t imrot, uint8_t imm8) {
+  inline void emit_alu_imm(armcg_regnum rd, armcg_regnum rn, uint32_t imrot, uint8_t imm8) {
     emit_aluop_imm<op, flg>(rd, rn, op2imm(imm8, imrot));
   }
 
   // 2 Operands (testing), always generates flags.
   template <armcg_op op>
-  inline void emit_test_reg_immshift(uint32_t rn, uint32_t rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
+  inline void emit_test_reg_immshift(armcg_regnum rn, armcg_regnum rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
     emit_aluop_reg<op, SetFlags>(0, rn, op2regimm(rm, smode, samount));
   }
   template <armcg_op op>
-  inline void emit_test_reg_regshift(uint32_t rn, uint32_t rm, uint32_t smode, uint32_t rs) {
+  inline void emit_test_reg_regshift(armcg_regnum rn, armcg_regnum rm, uint32_t smode, uint32_t rs) {
     emit_aluop_reg<op, SetFlags>(0, rn, op2regreg(rm, smode, rs));
   }
   template <armcg_op op>
-  inline void emit_test_imm(uint32_t rn, uint32_t imrot, uint8_t imm8) {
+  inline void emit_test_imm(armcg_regnum rn, uint32_t imrot, uint8_t imm8) {
     emit_aluop_imm<op, SetFlags>(0, rn, op2imm(imm8, imrot));
   }
 
   // Unary operations, ie. mov/mvn
   template <armcg_op op, FlagOperation flg>
-  inline void emit_mov_reg_immshift(uint32_t rd, uint32_t rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
+  inline void emit_mov_reg_immshift(armcg_regnum rd, armcg_regnum rm, uint32_t smode = ShiftLSL, uint32_t samount = 0) {
     emit_aluop_reg<op, flg>(rd, 0, op2regimm(rm, smode, samount));
   }
   template <armcg_op op, FlagOperation flg>
-  inline void emit_mov_reg_regshift(uint32_t rd, uint32_t rm, uint32_t smode, uint32_t rs) {
+  inline void emit_mov_reg_regshift(armcg_regnum rd, armcg_regnum rm, uint32_t smode, uint32_t rs) {
     emit_aluop_reg<op, flg>(rd, 0, op2regreg(rm, smode, rs));
   }
   template <armcg_op op, FlagOperation flg>
-  inline void emit_mov_imm(uint32_t rd, uint32_t imrot, uint8_t imm8) {
+  inline void emit_mov_imm(armcg_regnum rd, uint32_t imrot, uint8_t imm8) {
     emit_aluop_imm<op, flg>(rd, 0, op2imm(imm8, imrot));
   }
 
   // Just a regular operation, no shift, flags set (for easy thumb translation)
   template <armcg_op op>
-  inline void emit_alus_reg(uint32_t rd, uint32_t rn, uint32_t rm) {
+  inline void emit_alus_reg(armcg_regnum rd, armcg_regnum rn, armcg_regnum rm) {
     emit_aluop_reg<op, SetFlags>(rd, rn, op2regimm(rm, ShiftLSL, 0));
   }
   template <armcg_op op>
-  inline void emit_alus_imm(uint32_t rd, uint32_t rn, uint8_t imm8, uint8_t sa = 0) {
+  inline void emit_alus_imm(armcg_regnum rd, armcg_regnum rn, uint8_t imm8, uint8_t sa = 0) {
     emit_alu_imm<op, SetFlags>(rd, rn, sa, imm8);
   }
 
-  inline void emit_ldr_imm(uint32_t rd, uint32_t rb, uint16_t imm12) {
+  inline void emit_ldr_imm(armcg_regnum rd, armcg_regnum rb, uint16_t imm12) {
     emit_memop(rd, rb, 0x59, imm12);
   }
-  inline void emit_str_imm(uint32_t rd, uint32_t rb, uint16_t imm12) {
+  inline void emit_str_imm(armcg_regnum rd, armcg_regnum rb, uint16_t imm12) {
     emit_memop(rd, rb, 0x58, imm12);
   }
-  inline void emit_ldr_reg(uint32_t rd, uint32_t rb, uint32_t rm, uint32_t smode, uint32_t samount) {
+  inline void emit_ldr_reg(armcg_regnum rd, armcg_regnum rb, armcg_regnum rm, uint32_t smode, uint32_t samount) {
     emit_memop(rd, rb, 0x79, op2regimm(rm, smode, samount));
   }
-  inline void emit_str_reg(uint32_t rd, uint32_t rb, uint32_t rm, uint32_t smode, uint32_t samount) {
+  inline void emit_str_reg(armcg_regnum rd, armcg_regnum rb, armcg_regnum rm, uint32_t smode, uint32_t samount) {
     emit_memop(rd, rb, 0x78, op2regimm(rm, smode, samount));
   }
 
   // Other instructions
+  inline void emit_movw(armcg_regnum rd, uint16_t imm16) {
+    emit_inst(0xE, (rd << 12) | (imm16 & 0xFFF) | ((imm16 << 4) & 0xF0000) | 0x0300000);
+  }
+  inline void emit_movt(armcg_regnum rd, uint16_t imm16) {
+    emit_inst(0xE, (rd << 12) | (imm16 & 0xFFF) | ((imm16 << 4) & 0xF0000) | 0x0340000);
+  }
+
   template <armcg_mulopc opc, FlagOperation flg>
-  inline void emit_mull(uint32_t rdlo, uint32_t rdhi, uint32_t rn, uint32_t rm) {
+  inline void emit_mull(armcg_regnum rdlo, armcg_regnum rdhi, armcg_regnum rn, armcg_regnum rm) {
     emit_inst(0xE, 0x90 | rn | (rm << 8) | (rdlo << 12) | (rdhi << 16) | (flg == SetFlags ? (1<<20) : 0) | (opc << 21));
+  }
+  template <FlagOperation flg>
+  inline void emit_mla(armcg_regnum rd, armcg_regnum rm, armcg_regnum rs, armcg_regnum rn) {
+    emit_inst(0xE, 0x90 | rm | (rs << 8) | (rn << 12) | (rd << 16) | (flg == SetFlags ? (1<<20) : 0) | (armgc_mla << 21));
+  }
+  template <FlagOperation flg>
+  inline void emit_mul(armcg_regnum rd, armcg_regnum rm, armcg_regnum rs) {
+    emit_inst(0xE, 0x90 | rm | (rs << 8) | (rd << 16) | (flg == SetFlags ? (1<<20) : 0) | (armgc_mul << 21));
   }
 
 };
